26ade1d5c5de3f5bd0eb504893f0eb8b9afe9106
hyunsoo-jung <hyunsoo.jung@worksmobile.com>
2019-09-03 21:48:56 +0900

    2.라우팅 - 중첩 라우트

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      Card
+      <div>cid: {{cid}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.cid = this.$route.params.cid
+    },
+    watch: {
+        '$route'() {
+            this.cid = this.$route.params.cid
+        }
+    },
+    data() {
+        return {
+            cid: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
