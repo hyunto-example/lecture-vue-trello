1f80f250e29ecb47e41657cc6c6794bd46ee8df9
hyunsoo-jung <hyunsoo.jung@worksmobile.com>
2019-09-03 22:01:37 +0900

    2.라우팅 - 데이터 불러오기

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
       Card
-      <div>cid: {{cid}}</div>
+      <div v-if="loading">loading card...</div>
+      <div v-else>cid: {{cid}}</div>
   </div>
 </template>
 
 <script>
 export default {
-    created() {
-        this.cid = this.$route.params.cid
-    },
     watch: {
-        '$route'() {
-            this.cid = this.$route.params.cid
+        '$route': {
+            handler: 'fetchData',
+            immediate: true
         }
     },
     data() {
         return {
-            cid: 0
+            cid: 0,
+            loading: false
+        }
+    },
+    methods: {
+        fetchData() {
+            this.loading = true
+            setTimeout(() => {
+                this.cid = this.$route.params.cid
+                this.loading = false
+            }, 500)
         }
     }
 }
