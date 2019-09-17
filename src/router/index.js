50d608e4b4181aa27c9ca0e46c0953fc09526399
hyunto <jhyunto@gmail.com>
2019-09-17 23:35:35 +0900

    6. 상태관리 - Vuex로 인증 Flow 개선

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,19 @@ import Login from '../components/Login.vue'
 import Board from '../components/Board.vue'
 import Card from '../components/Card.vue'
 import NotFound from '../components/NotFound.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
 const requireAuth = (to, from, next) => {
-    const isAuth = localStorage.getItem('token')
     const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
-    isAuth ? next() : next(loginPath)
+    store.getters.isAuth ? next() : next(loginPath)
 }
 
 const router = new VueRouter({
     mode: 'history',
-    routes: [{
+    routes: [
+        {
             path: '/',
             component: Home,
             beforeEnter: requireAuth
